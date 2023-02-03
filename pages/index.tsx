d30f629fb116e7e2953fcc23830bf597057b0fa0
Michael Chop <007micle007@gmail.com>
2023-02-03 22:06:15 +0300

    Made the basic layout of the project

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>NextJS App</title>
+      </Head>
+
+      <main>
+
+      </main>
+    </>
+  )
+}
